bbc23c26098493af9c706de446f9a69b5aaf5bba
Donovan Fletcher <donovanfletcher29@gmail.com>
2024-05-20 10:34:32 -0400

    commit one

--- javascript.js
+++ javascript.js
@@ -1,29 +1,13 @@
+const apiKey= "2ab66c99a4a95153cb618340d97a0d3c";
 
-const apiKey="2ab66c99a4a95153cb618340d97a0d3c"
+let searchURL="http://api.openweathermap.org/data/2.5/forecast?q=Charlotte&appid=2ab66c99a4a95153cb618340d97a0d3c"
 
-const searchURL="http://api.openweathermap.org/data/2.5/forecast?q={city name},{state code},{country code}&appid=2ab66c99a4a95153cb618340d97a0d3c";
-
-
-
-function fetchweather (){
-    let apiKey="2ab66c99a4a95153cb618340d97a0d3c";
-    const city = document.getElementById('city').searchURL; }
-
-   
-
-    const currentWeatherUrl = 'https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${2ab66c99a4a95153cb618340d97a0d3c}';
-
-    const forecastUrl = 'https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${2ab66c99a4a95153cb618340d97a0d3c}';
-  
-  
-  function fetchweather() {
-    fetch(searchURL)
-.then(response => response.json())
-.then(data => console.log(data));
-document.getElementById("myBtn").addEventListener=("click",fetchweather);
-console.log(data)
+function fetchweather(){
+fetch(searchURL)
+.then(response=> response.json())
+.then(data=> console.log(data));
 }
-
+document.getElementById("myBtn").addEventListener("click",fetchweather);
 
 
 
